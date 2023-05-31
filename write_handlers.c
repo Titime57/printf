915d725dcedda75f5a44444f8935041671991069
Titime57 <fatimaezzahraa355@gmail.com>
2023-05-30 18:43:48 -0700

    printf

--- write_handlers.c
+++ write_handlers.c
@@ -84,7 +84,7 @@ int write_number(int is_negative, int ind, char buffer[],
  * @buffer: Buffer
  * @flags: Flags
  * @width: width
- * @precision: Precision specifier
+ * @prec: Precision specifier
  * @length: Number length
  * @padd: Pading char
  * @extra_c: Extra char
@@ -92,19 +92,18 @@ int write_number(int is_negative, int ind, char buffer[],
  * Return: Number of printed chars.
  */
 int write_num(int ind, char buffer[],
-	int flags, int width, int precision,
+	int flags, int width, int prec,
 	int length, char padd, char extra_c)
 {
 	int i, padd_start = 1;
 
-	if (precision == 0 && ind == BUFF_SIZE - 2 && buffer[ind]
-			== '0' && width == 0)
+	if (prec == 0 && ind == BUFF_SIZE - 2 && buffer[ind] == '0' && width == 0)
 		return (0); /* printf(".0d", 0)  no char is printed */
-	if (precision == 0 && ind == BUFF_SIZE - 2 && buffer[ind] == '0')
+	if (prec == 0 && ind == BUFF_SIZE - 2 && buffer[ind] == '0')
 		buffer[ind] = padd = ' '; /* width is displayed with padding ' ' */
-	if (precision > 0 && precision < length)
+	if (prec > 0 && prec < length)
 		padd = ' ';
-	while (precision > length)
+	while (prec > length)
 		buffer[--ind] = '0', length++;
 	if (extra_c != 0)
 		length++;
